cec52f29d9d4f8e147bb7a3c8d700a60c6a37b03
Felipeig2 <turipanes234@gmail.com>
2024-08-21 23:48:05 -0300

    adding ex6

--- Arrayx/ej6.c
+++ Arrayx/ej6.c
@@ -1,12 +1,31 @@
+/*. Escribir un programa que declare el array de enteros 1, 2, 3, 4, 5, 6 e invierta el orden del mismo. Luego imprimirlo.*/
+
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
+#include<string.h>
+void swap(int *num, int size){       
+int a = 0;
+
+for(int i = 0; i < size / 2; i++){
+  int temp = num[5-i];
+  num[5-i] = num[0+i];
+  num[0+i] = temp;
+}
+for(int i = 0; i < size; i++){
+  printf("%d ", num[i]);
+}
+}
+
 int main(int argc, char *argv[]){
-  int arrayx[] = {1, 2, 3, 4, 5, 6};
-  int numero = 0;
-  for(int i = 5; i >= 0; i--){
-    numero = arrayx[i];
-    printf("%d\n", numero);
-  }
-    return 1;
+int num[] = {1, 2, 3, 4, 5, 6};
+int size = 6;
+ printf("\n");
+for(int i = 0; i < size; i++){
+  printf("%d ", num[i]);
+}
+ printf("\n");
+swap(num, size);
+ printf("\n");
+
+return 0;
 }
