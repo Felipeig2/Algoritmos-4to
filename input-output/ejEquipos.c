e95d1024531cef3cef262d9531136ee9be3fe239
Felipeig2 <turipanes234@gmail.com>
2024-10-04 09:44:25 -0300

    adding ex finished

--- input-output/ejEquipos.c
+++ input-output/ejEquipos.c
@@ -2,35 +2,19 @@
 #include<stdlib.h>
 
 int main(void){
-    char linea[50];
     char equipos[15];
     int partidos_jugados = 0;
     int partidos_ganados = 0;
     int partidos_empatados = 0;
     int partidos_perdidos = 0;
+    int puntos = 0;
     FILE *fp = fopen("partidos.txt", "r");
 
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < 4; i++){
         fscanf(fp, "%d,%d,%d,%d,%s", &partidos_jugados, &partidos_ganados, &partidos_empatados, &partidos_perdidos, equipos);
-        printf("%d %d %d %d %s\n", partidos_jugados, partidos_ganados, partidos_empatados, partidos_perdidos, equipos);
+        puntos = partidos_ganados * 3 + partidos_empatados * 1;
+        printf("\n%s\t%d\t%d\t%d\t%d\t%d\n", equipos, partidos_jugados, partidos_ganados, partidos_empatados, partidos_perdidos, puntos);
     }
 
     return 0;
 }
-
-/*
-    for(int i = 0; i < 5; i++){
-            fscanf(fp,"%s", linea);
-            for(int i = 0; i < 50; i++){
-                if(linea[i] == ',' || linea[i] == 'G' ){
-                    break;
-                }
-                if()
-                printf("%c", linea[i]);
-
-            }
-            printf("\n");
-    }
-
-   
-*/
